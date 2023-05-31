0735bf38c5b8747209c382dad4a00e0be49b0023
minhhoccode111 <minhhoccode111@gmail.com>
2023-05-31 12:38:07 +0700

    Understand how this project works

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
-(function APP() {
-  window.addEventListener("DOMContentLoaded", () => {
-    //The only div in index.html
-    const content = document.getElementById("content");
-  });
-})();
+import "./style.css";
+import "./home.js";
+import "./menu.js";
+import "./contact.js";
+
+window.addEventListener("DOMContentLoaded", () => {
+  //The only div in index.html
+  const content = document.getElementById("content");
+  content.textContent = "Hello World";
+});
